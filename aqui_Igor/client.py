aa09f64d867547cc7b6bddb03d532c312fd3433f
Mc-Nanes <lon@poli.br>
2024-07-28 03:18:52 -0300

    sem erro de biblio

--- aqui_Igor/client.py
+++ aqui_Igor/client.py
@@ -48,9 +48,8 @@ class BettingClient:
 
         self.canvas = tk.Canvas(mid_wrapper, width=500, height=250, bg='black', highlightthickness=0)
         self.canvas.grid(row=0, column=0, columnspan=2)
-
         self.plane_image = Image.open("aqui_Igor/plane.png")
-        self.plane_image = self.plane_image.resize((50, 50), Image.ANTIALIAS)
+        self.plane_image = self.plane_image.resize((50, 50), Image.LANCZOS)
         self.plane_photo = ImageTk.PhotoImage(self.plane_image)
         self.plane_id = self.canvas.create_image(50, 125, image=self.plane_photo)
 
@@ -110,7 +109,6 @@ class BettingClient:
         elif message.startswith("CLIENTS_CONNECTED"):
             clients_connected = int(message.split()[1])
             print(f"Clients connected: {clients_connected}")
-
 
     def place_bet(self):
         bet_amount = self.bet_amount_var.get()
